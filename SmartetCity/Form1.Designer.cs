79e1d907bd4e106822238493c1ef3426e84533b0
Mx-AndradeG <85962319+Mx-AndradeG@users.noreply.github.com>
2021-06-15 14:07:37 -0500

    Add files via upload

---
+++ SmartetCity/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace SmartetCity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lumensAmbiente = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lumensFoco = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.estadoChido = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.onOffBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.estadoChido)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.onOffBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Smarted city";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Encendido/apagado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F);
+            this.label3.Location = new System.Drawing.Point(230, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Lumens del ambiente";
+            // 
+            // lumensAmbiente
+            // 
+            this.lumensAmbiente.AutoSize = true;
+            this.lumensAmbiente.Font = new System.Drawing.Font("Times New Roman", 14.25F);
+            this.lumensAmbiente.Location = new System.Drawing.Point(338, 122);
+            this.lumensAmbiente.Name = "lumensAmbiente";
+            this.lumensAmbiente.Size = new System.Drawing.Size(53, 21);
+            this.lumensAmbiente.TabIndex = 5;
+            this.lumensAmbiente.Text = "label4";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F);
+            this.label4.Location = new System.Drawing.Point(22, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Estado del alumbrado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 49);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Cerrar conexion";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F);
+            this.label5.Location = new System.Drawing.Point(230, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Lumens del foco";
+            // 
+            // lumensFoco
+            // 
+            this.lumensFoco.AutoSize = true;
+            this.lumensFoco.Font = new System.Drawing.Font("Times New Roman", 14.25F);
+            this.lumensFoco.Location = new System.Drawing.Point(326, 251);
+            this.lumensFoco.Name = "lumensFoco";
+            this.lumensFoco.Size = new System.Drawing.Size(53, 21);
+            this.lumensFoco.TabIndex = 12;
+            this.lumensFoco.Text = "label4";
+            this.lumensFoco.Click += new System.EventHandler(this.lumensFoco_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(299, 341);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 21);
+            this.label6.TabIndex = 16;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::SmartetCity.Properties.Resources.idea__1_;
+            this.pictureBox4.Location = new System.Drawing.Point(243, 229);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(69, 61);
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // estadoChido
+            // 
+            this.estadoChido.Image = global::SmartetCity.Properties.Resources.alerta;
+            this.estadoChido.Location = new System.Drawing.Point(58, 229);
+            this.estadoChido.Name = "estadoChido";
+            this.estadoChido.Size = new System.Drawing.Size(80, 68);
+            this.estadoChido.TabIndex = 9;
+            this.estadoChido.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::SmartetCity.Properties.Resources.sol;
+            this.pictureBox2.Location = new System.Drawing.Point(243, 102);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(71, 66);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // onOffBox
+            // 
+            this.onOffBox.Image = global::SmartetCity.Properties.Resources.switch_off;
+            this.onOffBox.Location = new System.Drawing.Point(49, 102);
+            this.onOffBox.Name = "onOffBox";
+            this.onOffBox.Size = new System.Drawing.Size(67, 53);
+            this.onOffBox.TabIndex = 1;
+            this.onOffBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(415, 453);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lumensFoco);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.estadoChido);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lumensAmbiente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.onOffBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.estadoChido)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.onOffBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox onOffBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lumensAmbiente;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox estadoChido;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label lumensFoco;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ SmartetCity/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartetCity
+{
+    public partial class Form1 : Form
+    {
+        System.IO.Ports.SerialPort PuertoEntrada;
+        bool abierto = true;
+        string onOff="";
+        int checarLumens=0;
+        int triger = 1;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            PuertoEntrada = new System.IO.Ports.SerialPort();
+            PuertoEntrada.PortName = "COM3";
+            PuertoEntrada.BaudRate = 9600;
+            PuertoEntrada.ReadTimeout = 500;
+            PuertoEntrada.Open();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Thread Hilo = new Thread(EvaluarInfo);
+            Hilo.Start();
+        }
+
+        private void EvaluarInfo() {
+
+            while (abierto)
+            {
+                try
+                {
+                    if (triger == 1)
+                    {
+                        triger = 2;
+                    }
+                    else
+                    {
+                        Thread.Sleep(500);
+                        string cadena = PuertoEntrada.ReadLine();
+                        char[] cadenaFracmentada = cadena.ToCharArray();
+
+                        for (int i = 0; i < cadenaFracmentada.Length; i++)
+                        {
+                            onOff += cadenaFracmentada[i];
+                        }
+
+                        cadena = "";
+                        if (cadenaFracmentada[0] == 'F')
+                        {
+                            for (int i = 1; i < cadenaFracmentada.Length; i++)
+                            {
+                                cadena += cadenaFracmentada[i];
+                            }
+                            lumensFoco.Invoke(new MethodInvoker(
+                             delegate
+                             {
+                                 lumensFoco.Text = cadena;
+                                 checarLumens = Convert.ToInt32(lumensFoco.Text);
+                             }
+                             ));
+
+                        }
+
+                        if (cadenaFracmentada[0] == 'S')
+                        {
+                            for (int i = 1; i < cadenaFracmentada.Length; i++)
+                            {
+                                cadena += cadenaFracmentada[i];
+                            }
+                            lumensAmbiente.Invoke(new MethodInvoker(
+                             delegate
+                             {
+                                 lumensAmbiente.Text = cadena;
+                             }
+                             ));
+                        }
+
+                        if (cadenaFracmentada[0] == 'P')
+                        {
+                            onOffBox.Image = Properties.Resources.switch_on;
+                            onOffBox.Refresh();
+                            onOffBox.Visible = true;
+
+                        }
+
+                        if (cadenaFracmentada[0] == 'A')
+                        {
+                            onOffBox.Image = Properties.Resources.switch_off;
+                            onOffBox.Refresh();
+                            onOffBox.Visible = true;
+                        }
+
+                        if (checarLumens > 960)
+                        {
+                            estadoChido.Image = Properties.Resources.palomita;
+                            onOffBox.Refresh();
+                            onOffBox.Visible = true;
+
+
+                        }
+
+                        if (checarLumens < 900 && checarLumens > 801)
+                        {
+                            estadoChido.Image = Properties.Resources.alerta;
+                            onOffBox.Refresh();
+                            onOffBox.Visible = true;
+                        }
+
+                        if (checarLumens < 800)
+                        {
+
+                            estadoChido.Image = Properties.Resources.tacha;
+                            onOffBox.Refresh();
+                            onOffBox.Visible = true;
+
+                        }
+
+                    }
+                }
+                catch
+                {
+
+                }
+
+            }
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lumensFoco_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aver_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
